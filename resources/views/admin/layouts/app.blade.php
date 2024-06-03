c45aaec554dea2bedab0a201a04c90769df59af1
Kaung Htet <kaunghtet22122003@gmail.com>
2024-06-03 21:56:51 +0630

    Admin Profile, Nav UI

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,8 +9,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        @yield('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-4 text-center bg">
+                <button class="btn bg-dark text-white w-50 my-2">Profile</button>
+                <button class="btn bg-dark text-white w-50 my-2">Admin List</button>
+                <button class="btn bg-dark text-white w-50 my-2">Category</button>
+                <button class="btn bg-dark text-white w-50 my-2">Post</button>
+                <button class="btn bg-dark text-white w-50 my-2">Trend Post</button>
+                <form method="POST" action="{{ route('logout')}}">
+                    @csrf
+                    <button class="btn bg-dark text-white w-50 my-2" type="submit">Log Out</button>
+                </form>
+            </div>
+            <div class="col">@yield('content')</div>
+        </div>
     </div>
 </body>
     {{-- Bootstrap Link --}}
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -3,5 +3,35 @@
 @section('title','My Media App')
 
 @section('content')
-    <h1 class="text-danger">This is admin Page!</h1>
+<form action="">
+    <div class="row">
+        <div class="col-7 offset-1 py-3">
+            <div class="form-group mt-3">
+                <label class="mb-2">Name</label>
+                <input type="text" class="form-control" placeholder="Enter Name ...">
+            </div>
+            <div class="form-group mt-3">
+                <label class="mb-2">Email</label>
+                <input type="email" class="form-control" placeholder="Enter Email ...">
+            </div>
+            <div class="form-group mt-3">
+                <label class="mb-2">Phone</label>
+                <input type="number" class="form-control" placeholder="Enter Phone ...">
+            </div>
+            <div class="form-group mt-3">
+                <label class="mb-2">Address</label>
+                <textarea class="form-control" cols="30" rows="5" placeholder="Enter Address ..."></textarea>
+            </div>
+            <div class="form-group mt-3">
+                <button class="btn btn-dark">Update</button>
+            </div>
+
+        </div>
+    </div>
+</form>
+<div class="row">
+    <div class="col-7 offset-1">
+        <a href="">Change Password</a>
+    </div>
+</div>
 @endsection
