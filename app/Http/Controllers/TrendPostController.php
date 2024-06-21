926fe8a6d20a83b245a3fb1be1e906c25de22ba1
Kaung Htet <kaunghtet22122003@gmail.com>
2024-06-22 00:13:27 +0630

    Action log list

--- app/Http/Controllers/TrendPostController.php
+++ app/Http/Controllers/TrendPostController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActionLog;
 use Illuminate\Http\Request;
 
 class TrendPostController extends Controller
 {
     //direct trend post page
     public function index(){
-        return view('admin.trend_post.index');
+        $posts = ActionLog::select('action_logs.*','posts.*')
+        ->leftJoin('posts','action_logs.post_id','posts.id')
+        ->paginate(5);
+        return view('admin.trend_post.index',compact('posts'));
     }
 }
--- resources/views/admin/trend_post/index.blade.php
+++ resources/views/admin/trend_post/index.blade.php
@@ -1,6 +1,62 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<h1>This is trend post page.</h1>
+<div class="col-12">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Order Table</h3>
+
+        <div class="card-tools">
+          <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+            <div class="input-group-append">
+              <button type="submit" class="btn btn-default">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body table-responsive p-0">
+        <table class="table table-hover text-nowrap text-center">
+          <thead>
+            <tr>
+              <th>Post ID</th>
+              <th>Post Title</th>
+              <th>Post Image</th>
+              <th>View Count</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->post_id}}</td>
+                <td>{{ $post->title }}</td>
+                <td>
+                    @if (!$post->image)
+                    <img width="100px" src="{{ asset('default_image.jpg')}}" class="img-thumbnail rounded shadow" alt="">
+                    @else
+                    <img width="100px" class="img-thumnail rounded shadow" src="{{ asset('postImage/'.$post->image)}}" alt="">
+                    @endif
+                </td>
+                <td class="d-flex justify-content-center align-items-center"><i class="fa-solid fa-eye mr-2"></i> <span>0</span></td>
+                <td>
+                  <button class="btn btn-sm bg-dark text-white"><i class="fa-solid fa-file"></i></button>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+        <div class="px-5">
+            {{ $posts->appends(request()->query())->links() }}
+        </div>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
 
 @endsection
